5c834e21b31e26a8d5cf6546a6b773eb2a26430d
Joshua Chang <joshua24322@gmail.com>
2018-10-07 21:55:10 +0800

    implement QRReader from https://youtu.be/FypyjizK_ww, and I tried it to workable

---
+++ QRReader/QRReader/WebViewController.swift
@@ -0,0 +1,37 @@
+//
+//  WebViewController.swift
+//  QRReader
+//
+//  Created by Joshua Chang on 2018/10/7.
+//  Copyright © 2018年 Joshua Chang. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+    
+    @IBOutlet var webView: UIWebView!
+    
+    var url = URL(string: "")
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let urlrequest = URLRequest(url: url!)
+        webView.loadRequest(urlrequest)
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
